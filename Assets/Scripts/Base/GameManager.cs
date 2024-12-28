cc1e2b1ffa0f42c3f80c79fd441d608992c9fc8f
Sayan <sayanguha.underpinservices@gmail.com>
2024-12-28 17:20:06 +0530

    fixed imgae issue

--- Assets/Scripts/Base/GameManager.cs
+++ Assets/Scripts/Base/GameManager.cs
@@ -527,6 +527,10 @@ public class GameManager : MonoBehaviour
     {
         audioController.StopSpinAudio();
         SingleLoopAnimation();
+        
+        if(SocketModel.resultGameData.freeSpinIndices.Count>0)
+        yield return new WaitForSeconds(1f);
+
         audioController.StopWLAaudio();
         uIManager.UpdatePlayerInfo(SocketModel.playerData);
 
@@ -579,6 +583,11 @@ public class GameManager : MonoBehaviour
             {
                 payLineController.GeneratePayline(SocketModel.resultGameData.linesToEmit[i]);
             }
+        }
+
+        if(SocketModel.resultGameData.freeSpinIndices.Count>0){
+             paylineSymbolAnimPanel.gameObject.SetActive(true);
+            slotManager.StartIconAnimation(SocketModel.resultGameData.freeSpinIndices, paylineSymbolAnimPanel);
         }
     }
 
